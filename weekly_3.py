123905492e6f7a85214c9b182fbd7902f42cbbf1
Termoplane <45878374+Termoplane@users.noreply.github.com>
2019-12-14 16:54:25 +0300

    исправил 3 задание

--- weekly_3.py
+++ weekly_3.py
@@ -2,7 +2,7 @@ num_1 = int(input())
 dic = [input().lower() for i in range(num_1)]  
 
 num_2 = int(input())
-values =[input().split() for j in range(num_2)]
+values =[input() for j in range(num_2)]
 
 ans = set()
 
