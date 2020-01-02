a93b31156e070ec7940fcfe18319027c4e855f56
Termoplane <45878374+Termoplane@users.noreply.github.com>
2020-01-03 01:10:09 +0300

    Дорешал поиск по графу своим методом

--- res_order_my_doreshat.py
+++ res_order_my_doreshat.py
@@ -1,31 +1,22 @@
-import sys
-sys.stdin = open("input.txt", "r")
-
 n = int(input())
 graph = {}
 
 for j in range(n):
     inp = input().split(' ')
-    graph[inp[0]] = [inp[0]]
+    graph[inp[0]] = []
     if ':' in inp:
         inp.remove(':')
     for j in inp[1:]:
         graph[inp[0]].append(j)
 
 def graph_measurer(a, b):
-    global graph
-    buf = []
-    if a in graph and b in graph:
-        for i in range(len(graph[b])):
-            buf.append(graph[b][i])
-            if a in graph[b]:
-                return True
-            elif i == len(graph[b]):
-                for k in range(len(buf)):
-                    return graph_measurer(a, buf[k])
-                buf.clear()
-            else:
-                continue
+    if a == b:
+        return True
+    for p in graph[b]:
+        if graph_measurer(a, p):
+            return True
+
+    return False 
         
 
 req_number = int(input())
