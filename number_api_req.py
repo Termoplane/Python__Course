424e2d76755fbac647585907bbcc0192f70a0031
Termoplane <45878374+Termoplane@users.noreply.github.com>
2020-01-09 12:27:35 +0300

    Update number_api_req.py

--- number_api_req.py
+++ number_api_req.py
@@ -1,15 +1,16 @@
 import requests
 import json
 
+params = {
+    'json': 'true'
+}
+
+
 with open ('dataset_24476_3.txt', 'r') as f:
     numbers = f.read().split()
 
 for number in numbers:
     api_url = f'http://numbersapi.com/{number}/math'
-
-    params = {
-        'json': 'true'
-    }
 
     res = requests.get(api_url, params=params)
 
